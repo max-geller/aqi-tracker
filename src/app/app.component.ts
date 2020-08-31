74dfa66de6a98e93f739b4d39b3521cd11723296
Max Geller <mxgeller@gmail.com>
2020-08-31 16:15:10 -0700

    initial styling, scripts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aqi-tracker';
+  // 1. import dependencies
+
+  // 2. pass then in constructor
+  constructor(
+    private renderer2: Renderer2,
+    @Inject(DOCUMENT) private _document
+  ) {
+  }
+  // 3. call them in ngOnInit
+  ngOnInit() {
+    const s1 = this.renderer2.createElement('script');
+    s1.type = 'text/javascript';
+    s1.src = 'https://www.purpleair.com/pa.widget.js?key=G0XSWU6X6D15O7VA&module=AQI&conversion=C0&average=10&layer=standard&container=PurpleAirWidget_19705_module_AQI_conversion_C0_average_10_layer_standard';
+    s1.text = ``;
+    this.renderer2.appendChild(this._document.body, s1);
+    const s2 = this.renderer2.createElement('script');
+    s2.type = 'text/javascript';
+    s2.src = 'https://www.purpleair.com/pa.widget.js?key=G0XSWU6X6D15O7VA&module=AQI&conversion=C0&average=10&layer=standard&container=PurpleAirWidget_19705_module_AQI_conversion_C0_average_10_layer_standard';
+    s2.text = ``;
+    this.renderer2.appendChild(this._document.body, s2);
+  }
 }
